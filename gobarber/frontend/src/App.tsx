841d90666c331261d2d789140fbf57c3453fe9ec
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-06-10 23:59:12 -0300

    signOut in auth hook

--- gobarber/frontend/src/App.tsx
+++ gobarber/frontend/src/App.tsx
@@ -5,7 +5,7 @@ import GlobalStyle from './styles/global';
 import SingIn from './pages/SignIn';
 import SingUp from './pages/SignUp';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
--- gobarber/frontend/src/pages/SignIn/index.tsx
+++ gobarber/frontend/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useContext } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -6,7 +6,7 @@ import * as Yup from 'yup';
 
 import { Container, Content, Background } from './styles';
 
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -22,7 +22,7 @@ interface SignInFormData {
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  const { signIn } = useContext(AuthContext);
+  const { signIn } = useAuth();
 
   const handerSubmit = useCallback(
     async (data: SignInFormData) => {
