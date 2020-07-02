c6f28615a7fda99097c694e6095c51e1a652f7e8
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-07-01 22:53:55 -0300

    Send mail tests

--- gobarber/backend/src/shared/container/providers/index.ts
+++ gobarber/backend/src/shared/container/providers/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
 
