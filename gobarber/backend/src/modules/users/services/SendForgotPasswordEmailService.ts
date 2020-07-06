4a1b7c47db3c482d80beb9705eb5229e1bffa536
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-07-06 06:00:02 -0300

    Send forgot password tests

--- gobarber/backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ gobarber/backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,6 +5,7 @@ import IMailProvider from '@shared/container/providers/MailProvider/models/IMail
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
 interface IRequestDTO {
   email: string;
@@ -14,10 +15,20 @@ interface IRequestDTO {
 class SendForgotPasswordEmailService {
   constructor(
     @inject('UsersRepository') private usersRepository: IUsersRepository,
-    @inject('MailProvider') private mailProvider: IMailProvider
+    @inject('MailProvider') private mailProvider: IMailProvider,
+    @inject('UserTokensRepository')
+    private userTokensProvider: IUserTokensRepository
   ) {}
 
   async execute({ email }: IRequestDTO): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    await this.userTokensProvider.generate(user.id);
+
     this.mailProvider.sendMail(email, 'Pedido de senha de email recebido!');
   }
 }
