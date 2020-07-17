5a154495d4fad4cce4dcd9c2f1e876be6a1f735d
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-07-16 22:39:07 -0300

    Using class transformer

--- gobarber/backend/src/modules/users/infra/http/controllers/ProfileController.ts
+++ gobarber/backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -24,7 +25,6 @@ export default class ProfileController {
       old_password,
       password,
     });
-    delete user.password;
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
