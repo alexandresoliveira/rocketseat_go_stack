d00939e2fe88710158d704dbf15d2db0e1296f85
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-07-25 15:08:37 -0300

    Add rate limiter from requests

--- gobarber/backend/src/shared/infra/http/server.ts
+++ gobarber/backend/src/shared/infra/http/server.ts
@@ -3,18 +3,20 @@ import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-import 'express-async-errors';
 import { errors } from 'celebrate';
+import 'express-async-errors';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadFolder));
