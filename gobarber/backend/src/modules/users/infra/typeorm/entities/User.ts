9d02466f434d912751088d98921e2b4679cfc9b7
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-07-24 21:00:34 -0300

    Add amazon s3 storage

--- gobarber/backend/src/modules/users/infra/typeorm/entities/User.ts
+++ gobarber/backend/src/modules/users/infra/typeorm/entities/User.ts
@@ -8,6 +8,8 @@ import {
 
 import { Exclude, Expose } from 'class-transformer';
 
+import uploadConfig from '@config/upload';
+
 @Entity('users')
 class Users {
   @PrimaryGeneratedColumn('uuid')
@@ -34,9 +36,14 @@ class Users {
 
   @Expose({ name: 'avatar_url' })
   getAvatarUrl(): string | null {
-    return this.avatar
-      ? `${process.env.APP_API_URL}/files/${this.avatar}`
-      : null;
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
 
