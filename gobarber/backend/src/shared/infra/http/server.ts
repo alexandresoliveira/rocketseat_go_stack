2f9027c80b66048093e690e2857551b2dd5af440
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-08-02 18:23:34 -0300

    Rate llimit after files route

--- gobarber/backend/src/shared/infra/http/server.ts
+++ gobarber/backend/src/shared/infra/http/server.ts
@@ -16,10 +16,10 @@ import routes from './routes';
 
 const app = express();
 
-app.use(rateLimiter);
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadFolder));
+app.use(rateLimiter);
 app.use(routes);
 app.use(errors());
 app.use(
