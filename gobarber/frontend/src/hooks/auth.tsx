ab2e9602bbc2ad607fb4d918f6671931287228ff
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-08-02 20:42:39 -0300

    Profile finish

--- gobarber/frontend/src/hooks/auth.tsx
+++ gobarber/frontend/src/hooks/auth.tsx
@@ -11,10 +11,12 @@ interface AuthContextData {
   user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(user: User): void;
 }
 
 interface User {
   id: string;
+  email: string;
   name: string;
   avatar_url: string;
 }
@@ -60,8 +62,21 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(
+    (user: User) => {
+      setData({
+        token: data.token,
+        user,
+      });
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+    },
+    [setData, data.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
